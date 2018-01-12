9b47adf835ec18faa597321332e828490a98785a
zyglroox <horseofbarberry67@gmail.com>
2018-01-13 00:43:30 +0300

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { HomeComponent } from './components/home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import appRoutes from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]

a8d4c3f26673267985c1bc9cef8725ecdb922788
zyglroox <horseofbarberry67@gmail.com>
2018-01-31 23:33:09 +0300

    handlebars fixed again + contact subscription

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/startWith';
@@ -12,15 +13,14 @@ import 'rxjs/add/operator/startWith';
 })
 export class ContactComponent implements OnInit {
   id;
-  contact$;
+  contact$ = new BehaviorSubject({name: 'Loading...', image: ''});
   constructor(private route: ActivatedRoute, private http: Http) {
     const apiUrl = 'http://localhost:3000/';
-    this.contact$ = route.params.
-      map((p: any) => p.id).
+    route.params.map((p: any) => p.id).
       switchMap(id => http.get(apiUrl + 'people/' + id).
       map(res => res.json())).
       map(contact => Object.assign({}, contact, {image: apiUrl + contact.image})).
-      startWith({name: 'Loading...', image: ''});
+      subscribe(this.contact$);
   }
 
   ngOnInit() {
